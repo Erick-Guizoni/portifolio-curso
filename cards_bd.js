81800a1536510b844e2c5996fad1483be0393a9e
Erick Guizoni <164954594+Erick-Guizoni@users.noreply.github.com>
2025-05-12 21:58:56 -0300

    mm

---
+++ cards_bd.js
@@ -0,0 +1,87 @@
+export const cards_bd = [
+    {
+      nome: 'To-Do List',
+      lenguages: 'JavaScript, HTML, CSS',
+      img: 'https://cdn-icons-png.flaticon.com/512/833/833472.png',
+    },
+    {
+      nome: 'Clima Tempo',
+      lenguages: 'JavaScript, API, Bootstrap',
+      img: 'https://cdn-icons-png.flaticon.com/512/869/869869.png',
+    },
+    {
+      nome: 'Calculadora Científica',
+      lenguages: 'Python (Tkinter)',
+      img: 'https://cdn-icons-png.flaticon.com/512/1055/1055644.png',
+    },
+    {
+      nome: 'Blog Pessoal',
+      lenguages: 'PHP, MySQL, HTML',
+      img: 'https://cdn-icons-png.flaticon.com/512/5953/5953462.png',
+    },
+    {
+      nome: 'Portfólio Interativo',
+      lenguages: 'React, Tailwind CSS',
+      img: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
+    },
+    {
+      nome: 'Chat em Tempo Real',
+      lenguages: 'Node.js, Socket.io',
+      img: 'https://cdn-icons-png.flaticon.com/512/2462/2462719.png',
+    },
+    {
+      nome: 'Jogo da Velha',
+      lenguages: 'JavaScript, HTML, CSS',
+      img: 'https://cdn-icons-png.flaticon.com/512/1006/1006555.png',
+    },
+    {
+      nome: 'Sistema de Login',
+      lenguages: 'Laravel (PHP), MySQL',
+      img: 'https://cdn-icons-png.flaticon.com/512/456/456212.png',
+    },
+    {
+      nome: 'Loja Virtual',
+      lenguages: 'Django, Python, SQLite',
+      img: 'https://cdn-icons-png.flaticon.com/512/1170/1170576.png',
+    },
+    {
+      nome: 'Relógio Digital',
+      lenguages: 'JavaScript, CSS',
+      img: 'https://cdn-icons-png.flaticon.com/512/992/992700.png',
+    },
+    {
+      nome: 'Dashboard de Vendas',
+      lenguages: 'React, Chart.js',
+      img: 'https://cdn-icons-png.flaticon.com/512/2784/2784459.png',
+    },
+    {
+      nome: 'Consumindo API de Filmes',
+      lenguages: 'Vue.js, Axios',
+      img: 'https://cdn-icons-png.flaticon.com/512/4069/4069620.png',
+    },
+    {
+      nome: 'Clone do Instagram',
+      lenguages: 'Flutter, Firebase',
+      img: 'https://cdn-icons-png.flaticon.com/512/2111/2111463.png',
+    },
+    {
+      nome: 'Conversor de Moedas',
+      lenguages: 'C#, .NET',
+      img: 'https://cdn-icons-png.flaticon.com/512/1792/1792688.png',
+    },
+    {
+      nome: 'Gerador de Senhas',
+      lenguages: 'Python, Tkinter',
+      img: 'https://cdn-icons-png.flaticon.com/512/561/561188.png',
+    },
+    {
+      nome: 'Página com Scroll Animado',
+      lenguages: 'HTML, CSS, GSAP',
+      img: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
+    },
+    {
+      nome: 'Checklist de Estudos',
+      lenguages: 'JavaScript, LocalStorage',
+      img: 'https://cdn-icons-png.flaticon.com/512/3909/3909444.png',
+    },
+  ];
